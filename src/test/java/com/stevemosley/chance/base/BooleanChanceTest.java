ff1b6dde4049a4cb5c63af38c839f71416a2fe50
smozely <me@steve-mosley.com>
2014-12-09 04:44:00 +1300

    Adding in some tests with assertj

--- src/test/java/com/stevemosley/chance/base/BooleanChanceTest.java
+++ src/test/java/com/stevemosley/chance/base/BooleanChanceTest.java
@@ -1,19 +1,27 @@
 package com.stevemosley.chance.base;
 
 import static com.stevemosley.chance.ChanceSettingsBuilder.aChanceSettings;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Repeat;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 public class BooleanChanceTest {
 
     private final BooleanChance underTest = new BooleanChance(aChanceSettings().build());
 
     @Test
+    @Repeat(1000)
     public void testBoolReturns() throws Exception {
-        assertThat(underTest.aBool(), anyOf(equalTo(true), equalTo(false)));
+        assertThat(underTest.aBool()).isNotNull().isIn(true, false);
     }
 
     @Test
@@ -24,14 +32,14 @@ public class BooleanChanceTest {
         // WHEN
         int trueCount = 0;
 
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 100000; i++) {
             if (underTest.aBool()) {
                 trueCount++;
             }
         }
 
         // THEN
-        assertThat(trueCount, allOf(greaterThan(4750), lessThan(5250)));
+        assertThat(trueCount).isGreaterThan(47500).isLessThan(52500);
     }
 
     @Test
@@ -42,14 +50,14 @@ public class BooleanChanceTest {
         // WHEN
         int trueCount = 0;
 
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             if (underTest.aBool(30)) {
                 trueCount++;
             }
         }
 
         // THEN
-        assertThat(trueCount, allOf(greaterThan(29500), lessThan(30500)));
+        assertThat(trueCount).isGreaterThan(295000).isLessThan(305000);
     }
 
 
